4011e0b9b7018f703bdc3783f945ed1b30acc876
Nabin Islam <sinabin62246@gmail.com>
2023-12-31 02:23:53 +0600

    user error handled

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,9 @@ const handleCreateUser = async (req, res, next) => {
     const userExists = await User.findOne({ email: email });
 
     if (userExists)
-      return res.status(400).json({ message: `User already exists` });
+      return res
+        .status(400)
+        .json({ success: false, message: `User already exists` });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -19,10 +21,50 @@ const handleCreateUser = async (req, res, next) => {
       image,
     });
 
-    return res.status(200).json({ message: `User created successfully`, user });
+    return res
+      .status(200)
+      .json({ success: true, message: `User created successfully`, user });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { handleCreateUser };
+const handleGetAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+
+    if (!users)
+      return res
+        .status(404)
+        .json({ success: false, message: `Users not found` });
+
+    return res.status(200).json({
+      success: true,
+      message: `Fetched all users successfully`,
+      users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const handleGetUserByEmail = async (req, res, next) => {
+  try {
+    const { email } = req.params;
+
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: `User not found` });
+
+    return res
+      .status(200)
+      .json({ success: true, message: `Fetched user successfully`, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { handleCreateUser, handleGetAllUsers, handleGetUserByEmail };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { handleCreateUser } = require('../controllers/userController');
+const {
+  handleCreateUser,
+  handleGetAllUsers,
+  handleGetUserByEmail,
+} = require('../controllers/userController');
 
 const userRouter = express.Router();
 
 userRouter.post('/', handleCreateUser);
+userRouter.get('/', handleGetAllUsers);
+userRouter.get('/:email', handleGetUserByEmail);
 
 module.exports = userRouter;
